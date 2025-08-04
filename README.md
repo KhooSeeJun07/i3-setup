f87d2ff146936fe3c7560266db39088f59870eaa
KhooSeeJun07 <khooseejun@outlook.com>
2025-08-04 21:59:10 +0800

    new file:   .config/cava/config 	new file:   .config/i3/config 	new file:   .config/i3/wallpaper/sunset-silhouettes-trees-mountains-generative-ai.jpg 	new file:   .config/picom/picom.conf 	new file:   .config/rofi/config.rasi 	modified:   README.md 	new file:   install.sh

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# i3-setup
+# i3wm Setup
+
+### Linux Distro
+[Arch Linux](https://archlinux.org)
+
+### Window Manager
+[i3wm](https://i3wm.org)
+
+### Terminal
+[Alacritty](https://alacritty.org)
+
+### App Launcher
+[Rofi](https://github.com/davatorium/rofi) \
+Theme: arthur by qball ( rofi-theme-selector )
+
+### Status Bar
+[Polybar](https://github.com/polybar/polybar) \
+Config: launch.sh --forest
+
+### Text editor
+[Neovim](https://neovim.io) \
+Config: [AstraNvim](https://astronvim.com)
---
+++ install.sh
@@ -0,0 +1,86 @@
+sudo pacman -Syu
+sudo pacman -S --needed --noconfirm \
+xorg-server \
+xorg-xinit \
+xorg-xrandr \
+xorg-xsetroot \
+i3-wm \
+picom \
+feh \
+alacritty \
+rofi \
+polybar \
+neovim \
+fastfetch \
+cava \
+noto-fonts \
+papirus-icon-theme
+
+mkdir -p ~/.config/i3
+cp .config/i3/config ~/.config/i3/
+cp .config/i3/wallpaper ~/.config/i3/
+mkdir -p ~/.config/picom
+cp .config/picom/picom.conf ~/.config/picom/
+mkdir -p ~/.config/rofi
+cp .config/rofi/config.rasi ~/.config/rofi/
+mkdir -p ~/.config/cava
+cp .config/cava/config ~/.config/cava/
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+read -p "Reboot now. (y/N): " choice
+case $choice in
+    y|Y)
+        sudo reboot
+        ;;
+    more)
+        echo "Continue for optional setup"
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+sudo pacman -S --needed --noconfirm \
+git \
+flatpak \
+pipewire \
+noto-fonts-cjk \
+mesa \
+lib32-mesa
+vulkan-radeon \
+lib32-vulkan-radeon
+libva-mesa-driver \
+mesa-vdpau \
+ffmpeg
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+
+sudo pacman -S --needed --noconfirm
+
+sudo pacman -S --needed --noconfirm\
+discord \
+jdk8-openjdk \
+jdk17-openjdk \
+jdk21-openjdk \
+prismlauncher \
+steam \
+proton-vpn-gtk-app \
+keepassxc \
+thunar \
+thunar-archive-plugin
+
+yay -S --needed --noconfirm \
+brave-bin \
+visual-studio-code-bin \
+localsend-bin
+
+flatpak install -y --noninteractive flathub \
+io.mrarm.mcpelauncher \
+org.vinegarhq.Sober
+
+
+
